1f0795ab7b8d942df667b941286dfaf03ab697cb
Stacy Curry <curry.stacy@gmail.com>
2017-06-07 20:56:52 -0500

    refactored solution to roman numerals challenge

--- algorithms-and-drills/roman-numerals/roman_numerals.rb
+++ algorithms-and-drills/roman-numerals/roman_numerals.rb
@@ -1,50 +1,42 @@
 def convert_to_roman(arabic_number, modern=false)
-  roman_numeral = ""
+  old_arabic_to_roman = { 1000  => "M",
+                          500   => "D",
+                          100   => "C",
+                          50    => "L",
+                          10    => "X",
+                          5     => "V",
+                          1     => "I" }
 
-  thousands = arabic_number/1000
-  five_hundreds = (arabic_number%1000)/500
-  hundreds = (arabic_number%500)/100
-  fifties = (arabic_number%100)/50
-  tens = (arabic_number%50)/10
-  fives = (arabic_number%10)/5
-  ones = (arabic_number%5)
+  new_arabic_to_roman = { 1000  => "M",
+                          900   => "CM",
+                          500   => "D",
+                          400   => "CD",
+                          100   => "C",
+                          90    => "XC",
+                          50    => "L",
+                          40    => "XL",
+                          10    => "X",
+                          9     => "IX",
+                          5     => "V",
+                          4     => "IV",
+                          1     => "I" }                          
 
-  if !modern
-    roman_numeral << "M" * thousands
-    roman_numeral << "D" * five_hundreds
-    roman_numeral << "C" * hundreds
-    roman_numeral << "L" * fifties
-    roman_numeral << "X" * tens
-    roman_numeral << "V" * fives
-    roman_numeral << "I" * ones
+  if modern
+    convert(arabic_number, new_arabic_to_roman)
   else
-    roman_numeral << "M" * thousands
+    convert(arabic_number, old_arabic_to_roman)
+  end
+end
 
-    if five_hundreds == 1 && hundreds == 4
-      roman_numeral << "CM"
-    elsif hundreds == 4
-      roman_numeral << "CD"
-    else
-      roman_numeral << "D" * five_hundreds
-      roman_numeral << "C" * hundreds
-    end
-
-    if fifties == 1 && tens == 4
-      roman_numeral << "XC"
-    elsif tens == 4
-      roman_numeral << "XL"
-    else
-     roman_numeral << "L" * fifties
-     roman_numeral << "X" * tens
-    end
-
-    if fives == 1 && ones == 4
-      roman_numeral << "IX"
-    elsif ones == 4
-      roman_numeral << "IV"
-    else
-      roman_numeral << "V" * fives
-      roman_numeral << "I" * ones
+def convert(arabic_number, conversion_hash)
+  roman_numeral = ""
+  remainder = arabic_number
+  
+  conversion_hash.each do |arabic, roman|
+    until remainder < arabic
+      roman_numeral << roman
+      remainder -= arabic
     end
   end
+  return roman_numeral
 end
